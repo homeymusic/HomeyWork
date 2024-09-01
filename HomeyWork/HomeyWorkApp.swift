71c21bc335bda4edc42597f9d704687aa8ac6306
Brian Mulloy <brian@landlessness.net>
2024-09-01 09:43:34 -0400

    Initial Commit

---
+++ HomeyWork/HomeyWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeyWorkApp.swift
+//  HomeyWork
+//
+//  Created by Brian McAuliff Mulloy on 9/1/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeyWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
